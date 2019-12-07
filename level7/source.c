3c7d38c0f3d299976b4035a92608b6eca30c7cdf
nicolasiragne <ahab@localhost.localdomain>
2019-12-07 13:32:52 +0100

    wip sources

--- level7/source.c
+++ level7/source.c
@@ -7,5 +7,25 @@ void m()
 
 int main(int ac, char **av)
 {
+	char*	s1;
+	char*	s2;
+	char*	s3;
+	char*	s4;
+	FILE*	f;
 
+	s1 = malloc(8);
+	s2 = malloc(8);
+	s3 = malloc(8);
+	s4 = malloc(8);
+	
+	s1[0] = 1;
+	s2[0] = 2;
+
+	strcpy(s3, av[1]);
+	strcpy(s4, av[2]);
+
+	f = fopen("/home/user/level8/.pass", "r");
+	fgets(flag, 68, f);
+	puts("~~");
+	return (0);
 }
